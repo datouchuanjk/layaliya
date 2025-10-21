d6f2ebf1fccc730151a16da0e5e8b59cea3725cc
datouchuanjk <13720327027@163.com>
2025-10-21 15:47:17 +0800

    first

--- module_wallet/src/main/java/com/module/wallet/viewmodel/DiamondViewModel.kt
+++ module_wallet/src/main/java/com/module/wallet/viewmodel/DiamondViewModel.kt
@@ -52,10 +52,11 @@ class DiamondViewModel(
         val id = pagingData.find { it.isSelected }?.id ?: return
         viewModelScope.launch {
             apiRequest {
-                api.buy(BuyRequest(id.toString())).checkAndGet()
+                api.buy(BuyRequest(id.toString())).checkAndGet() //创建订单接口
             }.apiResponse { result ->
+                Log.e("PayHelper", "跳去支付之前 先确定我这次购买的订单号是多少 ${result?.orderNum}")
                 PayHelper.pay(activity, result?.googleProductId.toString()) { successful, token ->
-                    Log.e("PayHelper", "支付完成 我回来了")
+                    Log.e("PayHelper", "支付完成 我回来了 这里我先输出 我购买接口 返回给我的订单号 ${result?.orderNum}")
                     if (successful) {
                         Log.e("PayHelper", "支付成功 开始调用验证接口")
                         verify(
@@ -107,7 +108,7 @@ class DiamondViewModel(
                     "验证接口调用成功  开始调用用户信息接口刷新砖石 现在的数量为${userInfo?.diamond}"
                 )
                 AppGlobal.userResponse(basicApi.user().checkAndGet())
-                Log.e("PayHelper", "验证接口调用成功  砖石数量为${userInfo?.diamond}")
+                Log.e("PayHelper", "用户信息接口调用成功  砖石数量为${userInfo?.diamond}")
             }.apiResponse(catch = { a, b ->
                 Log.e(
                     "PayHelper",
